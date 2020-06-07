d7a7de4a5b8bd6e4cf4048676b3002af7e979974
Adiraell <arayacastilloj@gmail.com>
2020-06-07 17:20:53 -0600

    Update Proyecto0.py

--- Proyecto0.py
+++ Proyecto0.py
@@ -6,13 +6,25 @@ print("   %%    %%%%%     %%    %%  %%    %%    %%%%%%")
 print("   %%    %%  %%    %%     %%%%     %%    %%  %%")
 print("   %%    %%  %%  %%%%%%    %%    %%%%%%  %%  %%")
 print("        										  ")
-<<<<<<< HEAD
-#comentario
 
-#Te parás >:v
-=======
 
 print("Descripción: Trivia es un juego en donde jugadores se reunen para ver quien sabe mas sobre un tema en especifico.")
 print("Reglas: ")
 print("1) Cada jugador debe responder 5 preguntas, durante 3 rondas \n   al final del juego el que haya acertado mas preguntas es el ganador.")
->>>>>>> upstream/master
+
+
+
+def cantidadjugadores():
+    #Funcion que le pide al usuario la cantidad de jugadores que van a jugar y sus respectivos nombres
+    #   Entrada: ninguna
+    #   Salida: La lista de jugadores que van a jugar
+    #   Restricciones: -La cantidad de jugadores debe de ser un entero positivo
+    listadejugadores = []
+    cantidad = int(input("Cuantos jugadores van a jugar: "))
+    while cantidad <= 0:
+        print("El numero de jugadores que ingreso es invalido. \nPorfavor ingrese un numero de jugadores valido")
+        cantidad = int(input("Cuantos jugadores van a jugar: "))
+    for jugador in range(cantidad):
+        listadejugadores.append(input("Ingrese el nombre del jugador "+ str(jugador + 1) + ": "))
+    
+    
