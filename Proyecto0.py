3f7bbf434c9669aa7efec2ef948efd868d67241f
Adiraell <arayacastilloj@gmail.com>
2020-06-07 20:27:40 -0600

    Update Proyecto0.py

--- Proyecto0.py
+++ Proyecto0.py
@@ -1,13 +1,13 @@
 #----------- Proyecto 0 - Michael Araya Murcia - Jose Julian Araya Castillo -------
 
-print(" %%%%%%  %%%%%   %%%%%%  %%  %%  %%%%%%   %%%% ")
-print("   %%    %%  %%    %%    %%  %%    %%    %%  %%")
-print("   %%    %%%%%     %%    %%  %%    %%    %%%%%%")
-print("   %%    %%  %%    %%     %%%%     %%    %%  %%")
-print("   %%    %%  %%  %%%%%%    %%    %%%%%%  %%  %%")
+print(" %%%%%%  %%%%%    %%%%%%  %%       %%  %%%%%%     %%%% ")
+print("      %%        %%      %%      %%        %%       %%       %%         %%     %%")
+print("      %%        %%%%%          %%        %%      %%       %%         %%%%%%")
+print("      %%        %%      %%       %%          %%%%           %%         %%      %%")
+print("      %%        %%      %%  %%%%%%       %%        %%%%%%    %%      %%")
 print("        										  ")
 
-<<<<<<< HEAD
+
 
 print("Descripción: Trivia es un juego en donde jugadores se reunen para ver quien sabe mas sobre un tema en especifico.")
 print("Reglas: ")
@@ -15,40 +15,19 @@ print("1) Cada jugador debe responder 5 preguntas, durante 3 rondas \n   al fina
 
 
 global listadejagadores
-def cantidadjugadores():
-    #Funcion que le pide al usuario la cantidad de jugadores que van a jugar y sus respectivos nombres
-    #   Entrada: ninguna
-    #   Salida: La lista de jugadores que van a jugar
-    #   Restricciones: -La cantidad de jugadores debe de ser un entero positivo
 
-    listadejugadores = []
-    cantidad = int(input("Cuantos jugadores van a jugar: "))
-    while cantidad <= 0:
-        print("El numero de jugadores que ingreso es invalido. \nPorfavor ingrese un numero de jugadores valido")
-        cantidad = int(input("Cuantos jugadores van a jugar: "))
-    for jugador in range(cantidad):
-        listadejugadores.append(input("Ingrese el nombre del jugador "+ str(jugador + 1) + ": "))
-    return listadejugadores
-#=====================================================va dentro del main=================================
-listadejugadores = cantidadjugadores()
-puntuaciones = []
 
-for jugador in listadejugadores:
-    puntuaciones.append(0)
-#====================================================================================================
 def puntuacion(listadejugadores, puntuaciones):
-    #
+    #Funcion que se encarga de imprimir la puntuacion en pantalla de cada jugador
     #
     #
     #
     i = 0
+    print("-"*100)
     for jugadores in listadejugadores:
-        print(str(listadejugadores[i])+": " , puntuaciones[i])
+        print("| "+str(listadejugadores[i])+": " , puntuaciones[i])
         i+= 1
-puntuacion(listadejugadores, puntuaciones)        
-    
-    
-=======
+    print("-"*100)
 def cantidadjugadores():
 	"""
 	Funcion que le pide al usuario la cantidad de jugadores que van a jugar y sus respectivos nombres.
@@ -74,6 +53,7 @@ def cantidadjugadores():
 
 	for jugador in range(cantidad):
 		listadejugadores.append(input("Ingrese el nombre del jugador "+ str(jugador + 1) + ": "))
+	return listadejugadores
 
 def abrirPreguntas():
 	archivo = open("prueba.json","r")
@@ -81,6 +61,12 @@ def abrirPreguntas():
 		print(linea)
 	archivo.close()
 
-cantidadjugadores()
-abrirPreguntas()
->>>>>>> upstream/master
+#=====================================================va dentro del main=================================
+listadejugadores = cantidadjugadores()
+puntuaciones = []
+
+for jugador in listadejugadores:
+    puntuaciones.append(0)
+#====================================================================================================	
+puntuacion(listadejugadores, puntuaciones)
+
